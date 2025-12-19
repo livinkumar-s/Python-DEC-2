fb23a1365a14f7ea14ac277533783c291a6bd606
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-19 11:56:52 +0530

    Introduction

---
+++ Intro.py
@@ -0,0 +1,12 @@
+#the below lines are to print out numbers
+'''Rashmika Mandanna (born 5 April 1996) is an Indian actress who primaily works in Telugu and Hindi films.
+Her accolades include four SIIMA Awards and a Filmfare Award South.
+One of South India's highest-paid actresses, she was featured in Forbes India's 2024 list of "30 Under 30".
+
+'''
+# print(100)
+# print(90)
+# print(80)
+
+a1=70
+print(a1)
