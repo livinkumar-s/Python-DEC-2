a86a36dac5dda4d1b9164aa3a0ad59646335ad92
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-22 11:33:53 +0530

    Datatypes and TypeCasting

---
+++ Datatypes.py
@@ -0,0 +1,36 @@
+# a=10
+# a=20
+# print(a)
+
+# a=210
+# b=3.14
+# c="Hello world"
+# d=True
+
+# print(a+b)
+# print("56"+"65")
+# print("Hello "+"World"+"!....")
+
+# name="Ajith"
+# age=52 #-->"52"
+
+# "His name is Ajith. His age is 52"
+
+# print("His name is " + name + ". His age is " + str(age))
+
+# print("Hello"+53)
+
+
+# a=input("Enter a value: ")
+
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+# print(a)
+
+# num=int(input("Enter a number: "))
+# print(100+num)
+
+# print(type(90)) #<class 'int'>
+# print(type('90')) #<class 'str'>
