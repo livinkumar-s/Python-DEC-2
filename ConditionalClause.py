744bfd1373bdd6527009b70b5f0115286da5d0e7
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-24 11:46:32 +0530

    Control Flow ---> Conditional clause

---
+++ ConditionalClause.py
@@ -0,0 +1,60 @@
+# age=int(input("Enter Your Age: "))
+# if age>=18:
+#     print("You can vote!")
+# else:
+#     print("Cannot Vote!")
+
+
+# if a>b:
+#     print("Step1")
+#     print("Step2")
+#     print("Step3")
+# a=-7
+# b=-6
+
+# if a<b:
+#     print("From If Block!")
+# else:
+#     print("From else block")
+
+# print("Final Step!")
+
+# j=67
+# k=76
+
+# if j>k:
+#     print("From If block!")
+# elif j==k:
+#     print("From Elif1 block")
+# elif j==k:
+#     print("From Elif2 block")
+
+# mark = int(input("Enter marks: "))
+
+# if mark > 90:
+#     print("Grade: O")
+# elif mark >= 81 and mark <= 90:
+#     print("Grade: A+")
+# elif mark >= 71 and mark <= 80:
+#     print("Grade: A")
+# elif mark >= 61 and mark <= 70:
+#     print("Grade: B")
+# elif mark >= 50 and mark <= 60:
+#     print("Grade: C")
+# elif mark >= 0 and mark <= 49:
+#     print("Grade: Fail")
+# else:
+#     print("Invalid marks")
+
+a=15
+
+if a>10:
+    if a>15:
+        print("Very large")
+    else:
+        print("large")
+else:
+    if a<5:
+        print("Very small")
+    else:
+        print("small")
