4690a85642c7eafb02b3d5800e6d1a31b1ce2f93
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-30 12:12:08 +0530

    Datastructures - List, Tuple

---
+++ Datastructures.py
@@ -0,0 +1,90 @@
+# a="Thalapathy"
+
+# print(a[-2])
+
+# print(a[-5:])
+# print(a[:-5])
+# print(a[:5])
+
+# print(a[3:7]) #3,4,5,6
+
+# print(a[1:8:2]) #1,3,5,7
+# print(a[::-1])
+
+
+# Datastructures 
+
+# List 
+
+
+# print(len(a))
+
+# print(a[6])
+# print(a[6:7])
+# a[10]=True
+
+# a.append("Last")
+# a.append("First")
+
+# a.extend([5,6,7,8])
+
+# a.append("Hello")
+
+# a.insert(-5,"Hello")
+
+# a.remove(4)
+# a.pop(3)
+# a.pop()
+# a.pop()
+# a.pop()
+# a.clear()
+
+# a=[3,4,5,6,7,"Hello",4,5,"Hi G",True,False]
+
+# a=[43,123,45,546,67,43,45,8,8]
+
+# print(a.index(44))
+# print(a.count(44))
+
+# a.sort()
+# a.reverse()
+# a[::-1]
+
+# print(a)
+
+# Tuple
+
+# tup1= (11,2,1,1,11,12,34)
+
+# tup1[3]=100
+
+# print(tup1[5])
+
+# print(tup1.index(11))
+# print(tup1.count(11))
+
+# person=(
+#     "Muthumani",
+#     90
+# )
+
+# name=person[0]
+# age=person[1]
+
+
+# person=(
+#     "Muthumani",
+#     90
+# )
+
+# name,age=person
+
+# print(name)
+# print(age)
+
+lis1=[1,2,3,[4,5,"six",["seven","eight","nine",["ten","eleven"]]]]
+
+# print(len(lis1))
+    
+# print(lis1[3][3][3][1])
+print(lis1[-1][-1][-1][-1][-1])
